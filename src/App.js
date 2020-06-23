9d3de95542e48bc3b20d393297a486e0516d4bc7
dodiameer <dodi@random-dodi.com>
2020-06-24 02:14:48 +0300

    Added <Footer/>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Header/>
+      <p>SOME CONTENT<br/><br/><br/></p>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <span>Copyright dodiameer&reg; 2004-2020. All rights reserved.</span>
+        </footer>
+    )
+}
+
+export default Footer
