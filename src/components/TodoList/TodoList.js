5e634577334329fa8f4d630c296ad86777050ed2
dodiameer <dodi@random-dodi.com>
2020-06-24 11:26:28 +0300

    Added state to <TodoList/>

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,6 +2,19 @@ import React from 'react'
 import './TodoList.css'
 
 class TodoList extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: []
+        }
+    }
+    
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(res => res.json())
+        .then(data => this.setState({todos: data}))
+    }
+
     render() {
         return (
             <div className="todo-list">
